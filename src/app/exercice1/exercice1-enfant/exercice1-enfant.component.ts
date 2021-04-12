6b0d0a0b095875e82de664295644ca1017561a99
ErwanFeltesse <er.feltesse@gmail.com>
2021-04-12 12:27:25 +0200

    exercice 1 done

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-exercice1-enfant',
-  templateUrl: './exercice1-enfant.component.html',
-  styleUrls: ['./exercice1-enfant.component.css']
+  selector: "app-exercice1-enfant",
+  templateUrl: "./exercice1-enfant.component.html",
+  styleUrls: ["./exercice1-enfant.component.css"],
 })
 export class Exercice1EnfantComponent implements OnInit {
+  @Input() public compteur!: number;
+  @Output() private setIncrement: EventEmitter<number> = new EventEmitter();
+  @Output() private setDecrement: EventEmitter<number> = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  public decrementer(compteur: number) {
+    this.setDecrement.emit(compteur);
   }
-
+  public incrementer(compteur: number) {
+    this.setIncrement.emit(compteur);
+  }
 }
--- src/app/exercice1/exercice1.component.ts
+++ src/app/exercice1/exercice1.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  OnChanges,
+  OnInit,
+} from "@angular/core";
 
 @Component({
-  selector: 'app-exercice1',
-  templateUrl: './exercice1.component.html',
-  styleUrls: ['./exercice1.component.css']
+  selector: "app-exercice1",
+  templateUrl: "./exercice1.component.html",
+  styleUrls: ["./exercice1.component.css"],
 })
 export class Exercice1Component implements OnInit {
-  public compteur: number = 0; 
+  public compteur: number = 0;
+  // Pour respecter le template html du composant, je crée la variable result
+  // on pourrait également appeler {{compteur}} à la place de {{result}} dans le template html
+  public result: number;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.result = this.compteur;
   }
-
+  incrementer() {
+    this.compteur++;
+    this.result = this.compteur;
+  }
+  decrementer() {
+    this.compteur--;
+    this.result = this.compteur;
+  }
 }
